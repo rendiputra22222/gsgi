f5d22c578644cdbc771c5bc0eae8fb50a74353cf
rendiputra22222 <rendiputra22222cxxx@gmail.com>
2025-03-11 19:01:11 +0700

    Update final.py

--- final.py
+++ final.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
 
 
 # Setup Selenium WebDriver
